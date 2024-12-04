451b5d43f021eb91c0b302bc91e9c26a73b849fa
iyedHM <163437000+iyedHM@users.noreply.github.com>
2024-12-04 09:17:55 +0100

    Update LCD.h

--- Our_Project_V1/LCD.h
+++ Our_Project_V1/LCD.h
@@ -1,37 +1,67 @@
 /*********************************************************************
- * @file  Apllication.h
- * @author <mettre l'adresse mail ou nom prenom>
- * @brief Fichier header de l'application
+ * @file  RgbLcd.cpp
+ * @author Hamrouni Iyed
+ * @brief Gestion d'un écran LCD RGB avec plusieurs fonctionnalités
  *********************************************************************/
-#ifndef APPLICATION_H_
-#define APPLICATION_H_
 
-/**
-  * @class Application
-  * @brief Classe Application 
-*/    
-class Application
-{
-  public :
+
+#include <rgb_lcd.h>
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+class LCD {
+private:
+    rgb_lcd lcd; // Instance de l'écran LCD
+    int red, green, blue; // Variables pour les couleurs RGB
+
+
+public:
+
+
     /**
-     * @fn Application();
+     * @fn Buzzer();
      * @brief Constructeur par defaut
     */    
-    Application();
+    LCD();
     /**
-     * @fn Application();
+     * @fn ~Buzzer();
      * @brief Destructeur
     */    
-    ~Application();    
+    ~LCD();    
     /**
      * @fn void init(void)
-     * @brief Fonction d'initialisation de l'application
+     * @brief Fonction d'initialisation du Buzzer
     */
-    void init(void);
+    void init();
     /**
-     * @fn void run(void)
-     * @brief Fonction de lancement de l'application
+     * @fn void on(void)
+     * @brief Fonction de lancement du Buzzer
     */
-    void run(void);
+    void setColor(int r, int g, int b);
+    /**
+     * @fn void off(void)
+     * @brief Fonction d'arret du Buzzer
+    */
+    void printMessage(const String &message, int line = 0);
+     /**
+     * @fn void on(void)
+     * @brief Fonction de lancement du Buzzer avec une frequence specifique
+    */
+    void clearScreen();
+     /**
+     * @fn void on(void)
+     * @brief Fonction de lancement du Buzzer avec une frequence specifique
+    */
+    void blinkText(const String &message, int duration, int interval = 500);
+     /**
+     * @fn void on(void)
+     * @brief Fonction de lancement du Buzzer pour une durée specifique
+    */
+    void displayAvailableColors();
+
+
 };
 #endif
